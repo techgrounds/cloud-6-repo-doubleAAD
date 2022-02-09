270d8b6ea823a96f2ddeb5f371610f46953b1659
doubleAAD <71566558+ahiesail@users.noreply.github.com>
2022-02-09 16:47:29 +0100

    Python

--- 03_Python/test.py
+++
@@ -1,6 +0,0 @@
-list = [25, 120, 55, 72, 89]
-for vol in range(len(list)):
-    if list[vol] == list[len(list)-1]:
-        print(list[vol] + list[0])
-    else:
-        print(list[vol] + list[vol+1])
